777b20837eab4300bf04a28b9b9771445853ac24
Fercho <fernandomatiaspessoa471@gmail.com>
2023-09-10 15:40:28 -0300

    create section-welcome

--- js/app.js
+++ js/app.js
@@ -1,4 +1,6 @@
 import toggleNav from "/js/components/toggleNav.js";
 
 const navBtn = document.getElementById("nav-btn");
+const modalnav = document.getElementById("modal-nav");
 navBtn.addEventListener("click", () => toggleNav());
+modalnav.addEventListener("click", () => toggleNav());
--- js/components/toggleNav.js
+++ js/components/toggleNav.js
@@ -1,10 +1,15 @@
 let navIsVisible = false;
 function toggleNav() {
   const nav = document.getElementById("nav");
+  const modalnav = document.getElementById("modal-nav");
   if (!navIsVisible) {
     nav.classList.remove("translate-x-[-100%]");
+    modalnav.classList.remove("hidden");
+    document.getElementById("body").style.overflowY = "hidden";
   } else {
     nav.classList.add("translate-x-[-100%]");
+    modalnav.classList.add("hidden");
+    document.getElementById("body").style.overflowY = "auto";
   }
   navIsVisible = !navIsVisible;
 }
