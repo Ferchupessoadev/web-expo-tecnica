45329caa619b9b83705a6a610e060d07a549ce29
Fercho <fernandomatiaspessoa471@gmail.com>
2023-09-13 06:56:48 -0300

    añadiendo detalles finales

--- js/app.js
+++ js/app.js
@@ -7,10 +7,18 @@ function toggleNav() {
     nav.classList.remove("translate-x-[-100%]");
     modalnav.classList.remove("hidden");
     document.getElementById("body").style.overflowY = "hidden";
+    document.getElementById("header").style.background =
+      "rgb(153 27 27 / 0.95)";
+    document.getElementById("header").style.opacity = "1";
   } else {
     nav.classList.add("translate-x-[-100%]");
     modalnav.classList.add("hidden");
     document.getElementById("body").style.overflowY = "auto";
+    if (window.scrollY > 0) {
+      document.getElementById("header").style.background = "rgb(0 0 0 / 0.7)";
+    } else {
+      document.getElementById("header").style.background = "transparent";
+    }
   }
   navIsVisible = !navIsVisible;
 }
@@ -27,17 +35,22 @@ addEventListener("scroll", () => {
 });
 
 const options = {
-  threshold: 0.6,
+  threshold: 0.5,
+};
+const optionsHome = {
+  threshold: 0.3,
 };
 const sectionHome = document.getElementById("section-inicio");
 const sectionDron = document.getElementById("section-dron");
 const sectionRobobloq = document.getElementById("section-robobloq");
 const sectionBrazo = document.getElementById("section-brazo");
+const setionSmartLed = document.getElementById("section-smart-led");
 function removeBtnActive() {
   document.getElementById("home-link").classList.remove("active");
   document.getElementById("dron-link").classList.remove("active");
   document.getElementById("robobloq-link").classList.remove("active");
   document.getElementById("brazo-link").classList.remove("active");
+  document.getElementById("smart-led-link").classList.remove("active");
 }
 
 const callbackObserver = (entries) => {
@@ -54,16 +67,39 @@ const callbackObserver = (entries) => {
         document.getElementById("robobloq-link").classList.add("active");
       } else if (seccion == "section-brazo") {
         document.getElementById("brazo-link").classList.add("active");
+      } else if (seccion == "section-smart-led") {
+        document.getElementById("smart-led-link").classList.add("active");
       }
-    } else {
-      document.getElementById("dron-link").classList.remove("active");
+    }
+  });
+};
+
+const callbackObserverHome = (entries) => {
+  entries.forEach((entry) => {
+    if (entry.isIntersecting) {
+      removeBtnActive();
+      let seccion = entry.target.getAttribute("id");
+      if (seccion == "section-inicio")
+        document.getElementById("home-link").classList.add("active");
     }
   });
 };
 
 let observer = new IntersectionObserver(callbackObserver, options);
+let observerHome = new IntersectionObserver(callbackObserverHome, optionsHome);
 
 observer.observe(sectionDron);
-observer.observe(sectionHome);
+observerHome.observe(sectionHome);
 observer.observe(sectionRobobloq);
 observer.observe(sectionBrazo);
+observer.observe(setionSmartLed);
+
+ScrollReveal().reveal(".img-secciones", { delay: 500 });
+
+const linksMobile = document.getElementsByClassName("link-mobile");
+
+for (const linkMobile of linksMobile) {
+  linkMobile.addEventListener("click", () => {
+    toggleNav();
+  });
+}
